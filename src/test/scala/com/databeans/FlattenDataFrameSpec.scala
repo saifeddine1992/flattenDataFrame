745a01427909a62ce83b328ae6ad9ce685263892
saifeddine1992 <saifeddinebouazizi1992@gmail.com>
2021-07-25 11:32:41 +0100

    flatten a dataframe and test

--- src/test/scala/com/databeans/FlattenDataFrameSpec.scala
+++ src/test/scala/com/databeans/FlattenDataFrameSpec.scala
@@ -8,12 +8,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 
+case class Data ( id: Long , data_created_at : Long , data_text : String , names_id : Long , names_hay_zouhour : String)
+
 class FlattenDataFrameSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
   implicit val spark: SparkSession = SparkSession
     .builder()
     .master("local[*]")
     .appName("Parser_Test")
     .getOrCreate()
+  import spark.implicits._
 
   "flattenDataFrame" should "Flatten the json DataFrame" in {
 
@@ -26,8 +29,7 @@ class FlattenDataFrameSpec extends AnyFlatSpec with Matchers with GivenWhenThen 
     val expectedResult = spark.read.option("multiLine", true).json("ParsedSample.json")
     val expectedSchema = expectedResult.schema
     expectedSchema should contain theSameElementsAs( schemaReturned )
+    expectedResult.as[Data].collect() should contain theSameElementsAs(parsedFile.as[Data].collect())
 
   }
-
-
 }
